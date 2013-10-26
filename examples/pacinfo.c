b6a67c084e3de41de792650ed06b10ebceec9fe6
Andrew Gregory <andrew.gregory.8@gmail.com>
2013-10-26 19:39:35 -0400

    pacinfo: use pu_find_pkgspec and add info
    Signed-off-by: Andrew Gregory <andrew.gregory.8@gmail.com>

--- examples/pacinfo.c
+++ examples/pacinfo.c
@@ -7,23 +7,33 @@
  *********************************************/
 
 int main(int argc, char **argv) {
-    struct pu_config_t *config = pu_config_new_from_file("/etc/pacman.conf");
-    alpm_handle_t *handle = pu_initialize_handle_from_config(config);
-    alpm_db_t *localdb = alpm_get_localdb(handle);
-    alpm_list_t *pkgs = alpm_db_get_pkgcache(localdb);
+	struct pu_config_t *config = pu_config_new_from_file("/etc/pacman.conf");
+	alpm_handle_t *handle = pu_initialize_handle_from_config(config);
 
-    for(; *argv; ++argv) {
-        alpm_pkg_t *p = alpm_find_satisfier(pkgs, *argv);
-        if(p) {
-            printf("%s %s\n", alpm_pkg_get_name(p), alpm_pkg_get_version(p));
-        }
-    }
+	for(++argv; *argv; ++argv) {
+		alpm_pkg_t *pkg = pu_find_pkgspec(handle, *argv);
+		if(!pkg) {
+			fprintf(stderr, "Unable to load package '%s'\n", *argv);
+			continue;
+		}
 
-    /* cleanup */
-    alpm_release(handle);
-		pu_config_free(config);
+		printf("Name:					%s\n", alpm_pkg_get_name(pkg));
+		printf("Version:			%s\n", alpm_pkg_get_version(pkg));
+		printf("Description:	%s\n", alpm_pkg_get_desc(pkg));
+		printf("Architecture: %s\n", alpm_pkg_get_arch(pkg));
+		printf("URL:					%s\n", alpm_pkg_get_url(pkg));
+		printf("Packager:			%s\n", alpm_pkg_get_packager(pkg));
 
-    return 0;
+		if(*(argv+1)) {
+			putchar('\n');
+		}
+	}
+
+	/* cleanup */
+	alpm_release(handle);
+	pu_config_free(config);
+
+	return 0;
 }
 
 /* vim: set ts=2 sw=2 noet: */
