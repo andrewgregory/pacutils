b5be92788222c6c1d68684109b043a78712c4b65
Andrew Gregory <andrew.gregory.8@gmail.com>
2013-10-26 20:25:57 -0400

    pacsync: set ALPM_DB_USAGE_ALL
    Signed-off-by: Andrew Gregory <andrew.gregory.8@gmail.com>

--- examples/pacsync.c
+++ examples/pacsync.c
@@ -181,6 +181,11 @@ int main(int argc, char **argv)
 		goto cleanup;
 	}
 
+	/* FIXME */
+	for(i = sync_dbs; i; i = i->next) {
+		alpm_db_set_usage(i->data, ALPM_DB_USAGE_ALL);
+	}
+
 	if(optind >= argc) {
 		targets = sync_dbs;
 	} else {
