ccdc351126e68a14b0be1615586a3b516463d41b
Andrew Gregory <andrew.gregory.8@gmail.com>
2014-06-29 14:46:05 -0400

    add siglevel check to config test
    Signed-off-by: Andrew Gregory <andrew.gregory.8@gmail.com>

--- t/pu_config.c
+++ t/pu_config.c
@@ -5,15 +5,42 @@
 
 #include "tap.h"
 
+#define is_list_exhausted(l, name) do { \
+        tap_ok(l == NULL, name " exhausted"); \
+        if(l) { \
+            tap_diag("remaining elements:"); \
+            while(l) { \
+                tap_diag(l->data); \
+                l = alpm_list_next(l); \
+            } \
+        } \
+    } while(0)
+
+
+#define is_str_list(l, str, desc) do { \
+        if(l) { \
+            tap_is_str(l->data, str, desc); \
+            l = alpm_list_next(l); \
+        } else { \
+            tap_ok(l != NULL, desc); \
+        } \
+    } while(0)
+
 int main(int argc, char **argv) {
     alpm_list_t *i;
+    pu_config_t *config;
+    pu_repo_t *repo;
     char temp_path1[] = "pu_config_test_XXXXXX";
     char temp_path2[] = "pu_config_test_XXXXXX";
     int fd1 = mkstemp(temp_path1);
     int fd2 = mkstemp(temp_path2);
     FILE *stream1 = fdopen(fd1, "r+");
     FILE *stream2 = fdopen(fd2, "r+");
-    fprintf(stream2, "XferCommand = included_xfercommand\n");
+    fprintf(stream2,
+            "XferCommand = included_xfercommand\n"
+            "[included repo]\n"
+            "SigLevel = PackageOptional\n"
+            "");
     fprintf(stream1,
             "\n"
             "[options]\n"
@@ -29,7 +56,6 @@ int main(int argc, char **argv) {
             "IgnorePkg = ignorepkgb\n"
             " IgnoreGroup = ignoregroupa ignoregroupb \n"
             "Architecture = i686\n"
-            "Include = %s\n"
             "NoUpgrade = /tmp/noupgrade*\n"
             "NoExtract = /tmp/noextract*\n"
             "CleanMethod = KeepInstalled KeepCurrent\n"
@@ -40,6 +66,9 @@ int main(int argc, char **argv) {
             "CheckSpace\n"
             "VerbosePkgLists\n"
             "ILoveCandy\n"
+            "SigLevel = Never\n"
+            "Include = %s\n"
+            "SigLevel = PackageRequired\n"
 
             "[core]\n"
             "Server = $repo:$arch\n"
@@ -49,9 +78,9 @@ int main(int argc, char **argv) {
     fclose(stream1);
     fclose(stream2);
 
-	tap_plan(29);
+	tap_plan(37);
 
-    pu_config_t *config = pu_config_new_from_file(temp_path1);
+    config = pu_config_new_from_file(temp_path1);
 
     tap_ok(config != NULL, "config != NULL");
     if(!config) {
@@ -75,26 +104,7 @@ int main(int argc, char **argv) {
     tap_ok(config->verbosepkglists, "VerbosePkgLists");
     tap_ok(config->ilovecandy, "ILoveCandy");
 
-#define is_list_exhausted(l, name) do { \
-        tap_ok(l == NULL, name " exhausted"); \
-        if(l) { \
-            tap_diag("remaining elements:"); \
-            while(l) { \
-                tap_diag(l->data); \
-                l = alpm_list_next(l); \
-            } \
-        } \
-    } while(0)
-
-
-#define is_str_list(l, str, desc) do { \
-        if(l) { \
-            tap_is_str(l->data, str, desc); \
-            l = alpm_list_next(l); \
-        } else { \
-            tap_ok(l != NULL, desc); \
-        } \
-    } while(0)
+    tap_is_int(config->siglevel, 0, "SigLevel");
 
     i = config->ignorepkgs;
     is_str_list(i, "ignorepkga", "IgnorePkg a");
@@ -127,6 +137,18 @@ int main(int argc, char **argv) {
     tap_is_float(config->usedelta, 0.7, 0.0001, "UseDelta (default)");
 
     tap_is_str(config->xfercommand, "included_xfercommand", "Include XferCommand");
+
+    tap_ok(config->repos != NULL, "repo list");
+
+    repo = config->repos->data;
+    tap_ok(repo != NULL, "included repo");
+    tap_is_str(repo->name, "included repo", "repo->name == 'included repo'");
+    tap_is_int(repo->siglevel, ALPM_SIG_PACKAGE, "[included repo] SigLevel");
+
+    repo = config->repos->next->data;
+    tap_ok(repo != NULL, "core");
+    tap_is_str(repo->name, "core", "repo->name == 'core'");
+    tap_is_str(repo->servers->data, "core:i686", "[core] server");
 
     unlink(temp_path1);
     unlink(temp_path2);
