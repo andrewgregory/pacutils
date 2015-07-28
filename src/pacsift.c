5e301df786ef92035202c4bb30084ca993047633
Andrew Gregory <andrew.gregory.8@gmail.com>
2015-07-28 19:47:56 -0400

    pacsift: add --root to usage
    Signed-off-by: Andrew Gregory <andrew.gregory.8@gmail.com>

--- src/pacsift.c
+++ src/pacsift.c
@@ -467,6 +467,7 @@ void usage(int ret)
 	hputs("options:");
 	hputs("   --config=<path>      set an alternate configuration file");
 	hputs("   --dbpath=<path>      set an alternate database location");
+	hputs("   --root=<path>        set an alternate installation root");
 	hputs("   --null[=sep]         use <sep> to separate values (default NUL)");
 	hputs("   --help               display this help information");
 	hputs("   --version            display version information");
